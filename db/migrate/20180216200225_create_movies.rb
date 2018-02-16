f9d40c35dbfbf13524f5e7e363526ffb841f2962
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-16 20:05:44 +0000

    automatically backed up by learn

--- db/migrate/20180216200225_create_movies.rb
+++ db/migrate/20180216200225_create_movies.rb
@@ -1,10 +1,4 @@
 class CreateMovies < ActiveRecord::Migration
   def change
-    add_column :movies, :title, :string 
-    add_column :movies
-    add_column :movies
-    add_column :movies
-    add_column :movies
-    add_column :movies
   end
 end
